1b5138e05352a19d78085ee6683056c03ddcc06e
Nikhil Bhadait <codesharad@gmail.com>
2024-09-08 16:28:49 +0530

    fix the bug

--- server/src/middlewares/admin.auth.middlewares.js
+++ server/src/middlewares/admin.auth.middlewares.js
@@ -1,23 +1,21 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 
-
 import { JWT_SECRET } from "../constant.js";
 
 const isAdminLoggedIn = asyncHandler(async (req, res, next) => {
-  
   // const adminToken = req.cookies ? req.cookies.adminToken : undefined;
   console.log("frontend cookies => ", req.cookies);
-  
-  const adminToken = req.cookies?.adminToken || req.header("Authorization")?.replace("Bearer ", "")
 
+  const adminToken =
+    req.cookies?.adminToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
 
   if (!adminToken) {
-
     return res
-    .status(200)
-    .redirect('http://localhost:5173/')
-    .json(new ApiResponse(400, "admin is not login in the server"))
+      .status(200)
+      .redirect("emp10.vercel.app/")
+      .json(new ApiResponse(400, "admin is not login in the server"));
   }
 
   try {
@@ -26,10 +24,9 @@ const isAdminLoggedIn = asyncHandler(async (req, res, next) => {
     req.user = decoded;
 
     console.log(req.user);
-    console.log("hello world")
+    console.log("hello world");
     next();
-  } 
-  catch (error) {
+  } catch (error) {
     console.log(error);
     return res.status(400).send(error.message);
   }
